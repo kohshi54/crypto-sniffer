7df4ff79923e07a890fdece752522f1cd32b5f63
kohshi54 <kohshi54.yam@gmail.com>
2024-10-02 07:07:26 -0400

    encryp/decrypt, before/after collect ok

--- trace.bpf.c
+++ trace.bpf.c
@@ -1,42 +1,55 @@
 #include <uapi/linux/ptrace.h>
 
+enum cipher_type_e {
+	ENCRYPT = 0,
+	DECRYPT = 1
+};
+
 struct data_t {
-	char buf[100];
+	enum cipher_type_e cipher_type;
+	char plain[100];
+	char encrypted[100];	
 };
 
 BPF_HASH(buf_addr, u32, u64); //local var no addr hozon
 BPF_PERF_OUTPUT(events);
+BPF_HASH(storage, u32, struct data_t); //暗号化前/暗号化後 or 復号前/暗号化後のデータを保存しておく, で関数exitのタイミングでPERF_OUTPUTに送信する
 
 int encrypt_update_enter(struct pt_regs *ctx, const void *evpctx, unsigned char *out, int *outl, unsigned char *in, int inl) {
 	if (!PT_REGS_PARM4(ctx)) // in
 		return 0;
-	
 	struct data_t data = {};
+	data.cipher_type = ENCRYPT;
 	//bpf_probe_read_user();
 	//bpf_probe_read(&data.buf, sizeof(data.buf), (void *)PT_REGS_PARM2(ctx));
-	bpf_probe_read(&data.buf, sizeof(data.buf), in);
+	bpf_probe_read(&data.plain, sizeof(data.plain), in);
 	//bpf_probe_read(&data.buf, inl > 100 ? 100 : inl, in);
 	//bpf_trace_printk("%s", data.buf);
-	events.perf_submit(ctx, &data, sizeof(data));
+	//events.perf_submit(ctx, &data, sizeof(data));
 
 	u32 pid = bpf_get_current_pid_tgid();
 	u64 param_addr = PT_REGS_PARM2(ctx);
 	buf_addr.update(&pid, &param_addr);
+	storage.update(&pid, &data);
 	bpf_trace_printk("encrypt_in: %d", pid);
 
 	return 0;
 };
 
 int encrypt_update_exit(struct pt_regs *ctx) {
-	struct data_t data = {};
 	u32 pid = bpf_get_current_pid_tgid();
 	u64 *param_addr = buf_addr.lookup(&pid);
 	if (param_addr == 0)
 		return 0;
-	bpf_probe_read_user(&data.buf, sizeof(data.buf), (void *)*param_addr);
-	events.perf_submit(ctx, &data, sizeof(data));
+	struct data_t *data = storage.lookup(&pid);
+	if (!data)
+		return 0;
+	bpf_probe_read_user(&data->encrypted, sizeof(data->encrypted), (void *)*param_addr);
+
+	events.perf_submit(ctx, data, sizeof(*data));
 
 	buf_addr.delete(&pid);
+	storage.delete(&pid);
 	bpf_trace_printk("encrypt_out: %d", pid);
 	return 0;
 }
@@ -45,55 +58,64 @@ int decrypt_update_enter(struct pt_regs *ctx, const void *evpctx, unsigned char 
 	if (!PT_REGS_PARM4(ctx)) // in
 		return 0;
 	struct data_t data = {0};
-	bpf_probe_read(&data.buf, sizeof(data.buf), in);
-	events.perf_submit(ctx, &data, sizeof(data));
+	data.cipher_type = DECRYPT;
+	bpf_probe_read(&data.encrypted, sizeof(data.encrypted), in);
+	//events.perf_submit(ctx, &data, sizeof(data));
 
 	u32 pid = bpf_get_current_pid_tgid();
 	u64 param_addr = PT_REGS_PARM2(ctx);
 	buf_addr.update(&pid, &param_addr);
+	storage.update(&pid, &data);
 	bpf_trace_printk("decrypt_in: %d", pid);
 	return 0;
 }
 
 int decrypt_update_exit(struct pt_regs *ctx) {
-	struct data_t data = {0};
 	u32 pid = bpf_get_current_pid_tgid();
 	u64 *param_addr = buf_addr.lookup(&pid);
 	if (param_addr == 0)
 		return 0;
-	bpf_probe_read_user(&data.buf, sizeof(data.buf), (void *)*param_addr);
-	events.perf_submit(ctx, &data, sizeof(data));
-	//data.buf[99] = '\0';	
+	struct data_t *data = storage.lookup(&pid);
+	if (!data)
+		return 0;
+	bpf_probe_read_user(&data->plain, sizeof(data->plain), (void *)*param_addr);
+	events.perf_submit(ctx, data, sizeof(*data));
 	buf_addr.delete(&pid);
+	storage.delete(&pid);
 	bpf_trace_printk("decrypt_out: %d", pid);
 	return 0;
 }
 
+///*
 int decrypt_finalex_enter(struct pt_regs *ctx, const void *evpctx, unsigned char *outm, int *outl) {
 	if (!PT_REGS_PARM2(ctx)) // buf mada encrypt
 		return 0;
 	struct data_t data = {0};
-	bpf_probe_read(&data.buf, sizeof(data.buf), outm);
-	events.perf_submit(ctx, &data, sizeof(data));
+	data.cipher_type = DECRYPT;
+	bpf_probe_read(&data.encrypted, sizeof(data.encrypted), outm);
+	//events.perf_submit(ctx, &data, sizeof(data));
 
 	u32 pid = bpf_get_current_pid_tgid();
 	u64 param_addr = PT_REGS_PARM2(ctx);
 	buf_addr.update(&pid, &param_addr);
+	storage.update(&pid, &data);
 	bpf_trace_printk("decrypt_finalin: %d", pid);
 	return 0;
 }
 
 int decrypt_finalex_exit(struct pt_regs *ctx) {
-	struct data_t data = {0};
 	u32 pid = bpf_get_current_pid_tgid();
 	u64 *param_addr = buf_addr.lookup(&pid);
 	if (param_addr == 0)
 		return 0;
-	bpf_probe_read_user(&data.buf, sizeof(data.buf), (void *)*param_addr);
-	events.perf_submit(ctx, &data, sizeof(data));
-	//data.buf[99] = '\0';	
+	struct data_t *data = storage.lookup(&pid);
+	if (!data)
+		return 0;
+	bpf_probe_read_user(&data->plain, sizeof(data->plain), (void *)*param_addr);
+	events.perf_submit(ctx, data, sizeof(*data));
 	buf_addr.delete(&pid);
+	storage.delete(&pid);
 	bpf_trace_printk("decrypt_finalout: %d", pid);
 	return 0;
 }
-
+//*/
--- trace.py
+++ trace.py
@@ -5,7 +5,14 @@ def print_event(cpu, data, size):
 	#event = ct.cast(data, ct.POINTER(Data)).contents
 	event = b["events"].event(data)
 	#print(f"{event.buf}")
-	printb(b"%s" % event.buf)
+	if event.cipher_type == 0:
+		print("encryption")
+		printb(b"before: %s" % event.plain)
+		printb(b"after: %s" % event.encrypted)
+	if event.cipher_type == 1:
+		print("decryption")
+		printb(b"before: %s" % event.encrypted)
+		printb(b"after: %s" % event.plain)
 
 #path="../openssl_evp_test/encrypt_test"
 path="/lib/x86_64-linux-gnu/libcrypto.so.3"
@@ -16,14 +23,13 @@ b.attach_uprobe(name=path, sym="EVP_EncryptUpdate", fn_name="encrypt_update_ente
 b.attach_uretprobe(name=path, sym="EVP_EncryptFinal_ex", fn_name="encrypt_update_exit")
 b.attach_uprobe(name=path, sym="EVP_DecryptUpdate", fn_name="decrypt_update_enter") # decryptupdateの段階でアドレス取っとく
 #b.attach_uretprobe(name=path, sym="EVP_DecryptUpdate", fn_name="decrypt_update_exit")
-#b.attach_uprobe(name=path, sym="EVP_DecryptFinal_ex", fn_name="decrypt_finalex_enter")
 b.attach_uretprobe(name=path, sym="EVP_DecryptFinal_ex", fn_name="decrypt_finalex_exit")
 #b.attach_uprobe(name="/usr/local/ssl/lib/libcrypto.so", sym="EVP_EncryptUpdate", fn_name="read_buf")
 b["events"].open_perf_buffer(print_event)
 while 1:
 	try:
 		b.perf_buffer_poll()
-		b.trace_print()
+		#b.trace_print()
 	except KeyboardInterrupt:
 		break
 
